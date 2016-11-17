39e466566f3707c2cbfb575ca5b5ff825bda0234
mmouterde <martin.mouterde@gmail.com>
2016-11-17 21:55:53 +0100

    fix build problem : upgrade to react-hot-loader: 3.0.0-beta.6

--- conf/webpack.conf.js
+++ conf/webpack.conf.js
@@ -6,59 +6,58 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
-  module: {
-    preLoaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint'
-      }
-    ],
+    module: {
+        preLoaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'eslint'
+            }
+        ],
 
-    loaders: [
-      {
-        test: /.json$/,
         loaders: [
-          'json'
+            {
+                test: /.json$/,
+                loaders: [
+                    'json'
+                ]
+            },
+            {
+                test: /\.(css|scss)$/,
+                loaders: [
+                    'style',
+                    'css',
+                    'sass',
+                    'postcss'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loaders: ['react-hot-loader/webpack',
+                    'babel'
+                ]
+            }
         ]
-      },
-      {
-        test: /\.(css|scss)$/,
-        loaders: [
-          'style',
-          'css',
-          'sass',
-          'postcss'
-        ]
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loaders: [
-          'react-hot',
-          'babel'
-        ]
-      }
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new HtmlWebpackPlugin({
+            template: conf.path.src('index.html')
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    postcss: () => [autoprefixer],
+    debug: true,
+    devtool: 'source-map',
+    output: {
+        path: path.join(process.cwd(), conf.paths.tmp),
+        filename: 'index.js'
+    },
+    entry: [
+        'webpack/hot/dev-server',
+        'webpack-hot-middleware/client',
+        `./${conf.path.src('index')}`
     ]
-  },
-  plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new HtmlWebpackPlugin({
-      template: conf.path.src('index.html')
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  postcss: () => [autoprefixer],
-  debug: true,
-  devtool: 'source-map',
-  output: {
-    path: path.join(process.cwd(), conf.paths.tmp),
-    filename: 'index.js'
-  },
-  entry: [
-    'webpack/hot/dev-server',
-    'webpack-hot-middleware/client',
-    `./${conf.path.src('index')}`
-  ]
 };
